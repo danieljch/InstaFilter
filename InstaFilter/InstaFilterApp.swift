5b54a6bdb5109a004809488fc868e73fe5dcaf55
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-14 16:41:34 -0400

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 14/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
